372b48b63af811f0a58b30ec9ea0cf6bd87c840c
atehio <a.tehio-pesselet@octo.com>
2022-06-29 16:18:05 +0200

    add features get balance & make deposit

--- bank/main.py
+++ bank/main.py
@@ -2,3 +2,9 @@ class BankAccount:
 
     def __init__(self):
         self.balance = 0
+
+    def get_balance(self) -> float:
+        return self.balance
+
+    def make_deposit(self, amount):
+        self.balance = amount
--- test/test_bank.py
+++ test/test_bank.py
@@ -9,12 +9,41 @@ def test_create_bank_account_with_ammout_at_Zero():
 
     # THEN
     assert bank_account.balance == 0
-    
+
+
+def test_given_bank_account_with_balance_zero_return_correct_balance():
+    # GIVEN
+    bank_account = BankAccount()
+
+    # WHEN-THEN
+    assert bank_account.get_balance() == 0
+
+
+def test_given_bank_account_balance_when_make_deposit_should_return_balance():
+    # GIVEN
+    bank_account = BankAccount()
+
+    # WHEN
+    bank_account.make_deposit(1000)
+
+    # THEN
+    assert bank_account.get_balance() == 1000
+
+
+def test_given_bank_account_balance_when_make_deposit_of_negative_value_should_throw_exception():
+    # GIVEN
+    bank_account = BankAccount()
+
+    # WHEN
+    bank_account.make_deposit(-2)
+
+    # THEN
+
 # DONE open a bank account with amount at 0
-# obtain balance in bank account
-# make deposit of 10 recorded with today's date
+# DONE obtain balance in bank account
+# DONE make deposit of 10 recorded /with today's date
 # cannot make deposit <= 0
-# make withdrawal of 5 recorded with today's date
+# make withdrawal of 5 recorded /with today's date
 # cannot make withdrawal <= 0
 # handle exception when insufficient balance for withdrawal
 # return list of all transactions sorted by date
